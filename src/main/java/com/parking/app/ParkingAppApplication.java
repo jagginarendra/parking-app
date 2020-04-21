a89c6065e5af43f2febc06ea72b93abea23e0012
jaggi <jagginarendra@gmail.com>
2020-04-21 12:23:16 +0530

    remove eureka client

--- src/main/java/com/parking/app/ParkingAppApplication.java
+++ src/main/java/com/parking/app/ParkingAppApplication.java
@@ -3,7 +3,6 @@ package com.parking.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
